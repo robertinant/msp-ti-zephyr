ca670e1dc16a6769d81139b939e5b2eded355217
a0272600 <luis.reynoso@ti.com>
2023-12-20 15:31:04 -0600

    Add OpenOCD support (in progress)

---
+++ boards/arm/lp_mspm0g3507/board.cmake
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=MSPM0G3507" "--speed=1000")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
